dfc4a1224c42aaa9459048437d52a0692d6a4adc
juanjo-geo <jjgrillom98@gmail.com>
2026-02-22 22:21:03 -0400

    Crear cities.js

---
+++ cities.js
@@ -0,0 +1,14 @@
+const allCities = [
+  { name: "Paris", country: "Francia", lat: 48.8566, lon: 2.3522 },
+  { name: "Madrid", country: "España", lat: 40.4168, lon: -3.7038 },
+  { name: "Bogotá", country: "Colombia", lat: 4.7110, lon: -74.0721 },
+  { name: "New York", country: "USA", lat: 40.7128, lon: -74.0060 },
+  { name: "Tokyo", country: "Japón", lat: 35.6762, lon: 139.6503 },
+  { name: "Roma", country: "Italia", lat: 41.9028, lon: 12.4964 },
+  { name: "Berlín", country: "Alemania", lat: 52.5200, lon: 13.4050 },
+  { name: "Lima", country: "Perú", lat: -12.0464, lon: -77.0428 },
+  { name: "Santiago", country: "Chile", lat: -33.4489, lon: -70.6693 },
+  { name: "Toronto", country: "Canadá", lat: 43.65107, lon: -79.347015 },
+  { name: "Sydney", country: "Australia", lat: -33.8688, lon: 151.2093 },
+  { name: "El Cairo", country: "Egipto", lat: 30.0444, lon: 31.2357 }
+];
